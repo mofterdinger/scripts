07134ffa5c0fc1bef004ee7ab60bcc9bab128872
mofterdinger <mofterdinger@users.noreply.github.com>
2018-12-12 11:58:55 +0100

    home size 250G

--- archlinux/install_light.sh
+++ archlinux/install_light.sh
@@ -63,7 +63,7 @@ vgdisplay
 #########################
 lvcreate --name swap -L 16G $VOL_GRP
 lvcreate --name root -L 25G $VOL_GRP
-lvcreate --name home -l 100%FREE $VOL_GRP
+lvcreate --name home -l 250G $VOL_GRP
 lvdisplay
 
 #########################
