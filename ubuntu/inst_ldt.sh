0e30153a346c4e69a85f36c2b2355f158622120f
mofterdinger <mofterdinger@users.noreply.github.com>
2019-01-22 20:08:42 +0100

    add openssh-server

--- ubuntu/inst_ldt.sh
+++ ubuntu/inst_ldt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-sudo apt-get install curl htop deborphan samba libnss3-tools
+sudo apt-get install curl htop deborphan samba openssh-server libnss3-tools
 
 wget https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt-support.sh
 chmod 755 ldt-support.sh
