a2c849c9dc595734b846321e0995f0a586e122c3
mofterdinger <mofterdinger@users.noreply.github.com>
2019-01-23 08:59:21 +0100

    --no-check-certificate

--- ubuntu/inst_ldt.sh
+++ ubuntu/inst_ldt.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install curl aptitude htop deborphan samba openssh-server libnss3-tools
 
-wget -O ldt.certificates.sh https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt.certificates.sh
+wget -O ldt.certificates.sh https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt.certificates.sh --no-check-certificate
 chmod +x ldt.certificates.sh
-sudo ldt.certificates.sh
+sudo ./ldt.certificates.sh
 
-wget -O ldt-support.sh https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt-support.sh
+wget -O ldt-support.sh https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt-support.sh --no-check-certificate
 chmod +x ldt-support.sh
