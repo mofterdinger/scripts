e615007ae3b265bf6e7f21e3f74e3caabfd483ec
mofterdinger <mofterdinger@users.noreply.github.com>
2019-01-23 08:31:36 +0100

    Update inst_ldt.sh

--- ubuntu/inst_ldt.sh
+++ ubuntu/inst_ldt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -x
 
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install curl htop deborphan samba openssh-server libnss3-tools
 
 wget -O ldt.certificates.sh https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt.certificates.sh
 chmod +x ldt.certificates.sh
+sudo ldt.certificates.sh
 
 wget -O ldt-support.sh https://linuxinfra.wdf.sap.corp/ldt/scripts/ldt-support.sh
 chmod +x ldt-support.sh
