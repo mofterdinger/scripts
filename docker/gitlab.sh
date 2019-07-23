311ac60e61163c1d3c69bc3b022995315a833671
markus <markus.ofterdinger@sap.com>
2019-07-23 17:54:00 +0200

    fixed gitlab

--- docker/gitlab.sh
+++ docker/gitlab.sh
@@ -2,7 +2,7 @@
 
 sudo docker run --detach \
   --hostname wdfl33450964a.dhcp.wdf.sap.corp \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --publish 443:443 --publish 80:80 --publish 1022:22 \
   --name gitlab \
   --restart always \
   --volume /srv/gitlab/config:/etc/gitlab \
