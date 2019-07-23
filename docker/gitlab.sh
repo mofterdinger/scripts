d062e4238b913ffd46ddece0e9542c182f82aa05
Markus Ofterdinger <markus.ofterdinger@sap.com>
2019-07-23 17:56:11 +0200

    gitlab

--- docker/gitlab.sh
+++ docker/gitlab.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -x
 
-sudo docker run --detach \
+NAME=gitlab
+
+docker rm $NAME
+
+docker run --detach \
   --hostname wdfl33450964a.dhcp.wdf.sap.corp \
   --publish 443:443 --publish 80:80 --publish 1022:22 \
-  --name gitlab \
+  --name $NAME \
   --restart always \
   --volume /srv/gitlab/config:/etc/gitlab \
   --volume /srv/gitlab/logs:/var/log/gitlab \
