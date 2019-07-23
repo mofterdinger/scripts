eaa6523223c95ceb0e2ca15949118fd7209d5c64
markus <markus.ofterdinger@sap.com>
2019-07-23 18:05:42 +0200

    fixed startup issue

--- docker/gitlab.sh
+++ docker/gitlab.sh
@@ -3,6 +3,8 @@ set -x
 
 NAME=gitlab
 
+docker stop $NAME
+
 docker rm $NAME
 
 docker run --detach \
@@ -10,7 +12,7 @@ docker run --detach \
   --publish 443:443 --publish 80:80 --publish 1022:22 \
   --name $NAME \
   --restart always \
-  --volume /srv/gitlab/config:/etc/gitlab \
-  --volume /srv/gitlab/logs:/var/log/gitlab \
-  --volume /srv/gitlab/data:/var/opt/gitlab \
+  --volume /srv/gitlab/config:/etc/gitlab:Z \
+  --volume /srv/gitlab/logs:/var/log/gitlab:Z \
+  --volume /srv/gitlab/data:/var/opt/gitlab:Z \
   gitlab/gitlab-ce:latest
