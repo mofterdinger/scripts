f87912bdb7a625da63d7beee7e2b6d852b2a050d
markus <markus.ofterdinger@sap.com>
2019-07-26 12:27:56 +0200

    fixes

--- docker/freeipa.sh
+++ docker/freeipa.sh
@@ -2,21 +2,21 @@
 set -x
 
 NAME=freeipa-server
-SERVER_PASSWORD=password
-ADMINT_PASSWORD=admin-password
+SERVER_PASSWORD=ds-password
+ADMIN_PASSWORD=admin-password
 
 docker stop $NAME
 
-docker rm $NAME
-
-docker run --name $NAME -ti \
+docker run --rm -ti \
+   --name $NAME \
    -h ipa.example.test \
    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
    --tmpfs /run \
    --tmpfs /tmp \
    -v /srv/freeipa/data:/data:Z \
-   --realm=example.test \
-   --tmpfs /tmp \
-   --password=$SERVER_PASSWORD \
+   freeipa/freeipa-server \
+   exit-on-finished \
+   -U \
+   --ds-password=$SERVER_PASSWORD \
    --admin-password=$ADMIN_PASSWORD \
-   freeipa/freeipa-server
+   --realm=example.test
