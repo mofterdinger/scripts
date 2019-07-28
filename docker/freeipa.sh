6e7b4cb32fa30526b864d612f6f85fa09be81f2b
Markus Ofterdinger <markus@ofterdinger.de>
2019-07-28 17:52:45 +0200

    publish more ports

--- docker/freeipa.sh
+++ docker/freeipa.sh
@@ -10,9 +10,13 @@ SERVER_IP=127.0.0.1
 docker stop $NAME
 
 docker run --rm \
-  --name $NAME \
-  --env  IPA_SERVER_IP=$SERVER_IP \
-  --publish  80:80 -p 443:443 -p 389:389 -p 636:636 \
+  --name     $NAME \
+  --env      IPA_SERVER_IP=$SERVER_IP \
+  --publish  80:80 -p 443:443 \
+  --publish  389:389 -p 636:636 \
+  --publish  88:88 -p 464:464 -p 88:88/udp -p 464:464/udp \
+  --publish  123:123/udp \
+  --publish  9443:9443 -p 9444:9444 -p 9445:9445 \
   --hostname $HOSTNAME \
   --tmpfs    /run \
   --tmpfs    /tmp \
