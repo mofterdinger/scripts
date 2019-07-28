d36c831c8995aabab69e0c05147e8b67627f973a
Markus Ofterdinger <markus@ofterdinger.de>
2019-07-28 14:44:36 +0200

    fixed ipv6

--- docker/freeipa.sh
+++ docker/freeipa.sh
@@ -8,15 +8,16 @@ ADMIN_PASSWORD=admin-password
 docker stop $NAME
 
 docker run --rm -ti \
-   --name $NAME \
-   -h ipa.example.test \
-   -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-   --tmpfs /run \
-   --tmpfs /tmp \
-   -v /srv/freeipa/data:/data:Z \
-   freeipa/freeipa-server \
-   exit-on-finished \
-   -U \
-   --ds-password=$SERVER_PASSWORD \
-   --admin-password=$ADMIN_PASSWORD \
-   --realm=example.test
+  --name $NAME \
+  -h ipa.example.test \
+  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+  --tmpfs /run \
+  --tmpfs /tmp \
+  -v /srv/freeipa/data:/data:Z \
+  --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+  freeipa/freeipa-server \
+  exit-on-finished \
+  -U \
+  --ds-password=$SERVER_PASSWORD \
+  --admin-password=$ADMIN_PASSWORD \
+  --realm=example.test
