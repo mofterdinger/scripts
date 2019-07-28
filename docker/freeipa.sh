b6dd471ce2385bd5f24ff647cc8cdc3376a488e4
Markus Ofterdinger <markus@ofterdinger.de>
2019-07-28 15:32:02 +0200

    freeipa fixes

--- docker/freeipa.sh
+++ docker/freeipa.sh
@@ -4,17 +4,22 @@ set -x
 NAME=freeipa-server
 SERVER_PASSWORD=ds-password
 ADMIN_PASSWORD=admin-password
+HOSTNAME=ipa.example.test
+SERVER_IP=127.0.0.1
 
 docker stop $NAME
 
-docker run --rm -ti \
+docker run --rm \
   --name $NAME \
-  -h ipa.example.test \
-  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-  --tmpfs /run \
-  --tmpfs /tmp \
-  -v /srv/freeipa/data:/data:Z \
-  --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+  --env  IPA_SERVER_IP=$SERVER_IP \
+  --publish  80:80 -p 443:443 \
+  --hostname $HOSTNAME \
+  --tmpfs    /run \
+  --tmpfs    /tmp \
+  --volume   /sys/fs/cgroup:/sys/fs/cgroup:ro \
+  --volume   /srv/freeipa/data:/data:Z \
+  --sysctl   net.ipv6.conf.all.disable_ipv6=0 \
+  --detach \
   freeipa/freeipa-server \
   exit-on-finished \
   -U \
